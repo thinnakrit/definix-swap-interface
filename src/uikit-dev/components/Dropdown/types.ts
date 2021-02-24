d0a21fdc41360876c631b14f80018a4c29a4750c
Laddawal Kongwong <laddawal.kongwong>
2021-02-24 17:36:19 +0700

    Account Dropdown

--- src/uikit-dev/components/Dropdown/types.ts
+++ src/uikit-dev/components/Dropdown/types.ts
@@ -1,9 +1,10 @@
-export type Position = "top" | "top-right" | "bottom";
+export type Position = 'top' | 'top-right' | 'bottom' | 'bottom-right'
 
 export interface PositionProps {
-  position?: Position;
+  position?: Position
 }
 
 export interface DropdownProps extends PositionProps {
-  target: React.ReactElement;
+  target: React.ReactElement
+  isRainbow?: boolean
 }
--- src/uikit-dev/widgets/Menu/UserBlock.tsx
+++ src/uikit-dev/widgets/Menu/UserBlock.tsx
@@ -1,41 +1,74 @@
-import React from "react";
-import Button from "../../components/Button/Button";
-import { useWalletModal } from "../WalletModal";
-import { Login } from "../WalletModal/types";
+import React from 'react'
+import { Heading } from 'uikit-dev'
+import Button from '../../components/Button/Button'
+import Dropdown from '../../components/Dropdown/Dropdown'
+import LinkExternal from '../../components/Link/LinkExternal'
+import { useWalletModal } from '../WalletModal'
+import { localStorageKey } from '../WalletModal/config'
+import CopyToClipboard from '../WalletModal/CopyToClipboard'
+import { Login } from '../WalletModal/types'
 
 interface Props {
-  account?: string;
-  login: Login;
-  logout: () => void;
+  account?: string
+  login: Login
+  logout: () => void
 }
 
 const UserBlock: React.FC<Props> = ({ account, login, logout }) => {
-  const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account);
-  const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null;
+  const { onPresentConnectModal } = useWalletModal(login, logout, account)
+  const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null
+  const accountEllipsisLong = account ? `${account.substring(0, 12)}...${account.substring(account.length - 12)}` : null
+
   return (
     <div>
       {account ? (
-        <Button
-          size="sm"
-          variant="tertiary"
-          onClick={() => {
-            onPresentAccountModal();
-          }}
+        <Dropdown
+          position="bottom-right"
+          isRainbow
+          target={
+            <Button
+              size="sm"
+              variant="tertiary"
+              // onClick={() => {
+              //   onPresentAccountModal()
+              // }}
+            >
+              {accountEllipsis}
+            </Button>
+          }
         >
-          {accountEllipsis}
-        </Button>
+          <div className="pa-2">
+            <Heading className="mb-4">{accountEllipsisLong}</Heading>
+            <LinkExternal isIconLeft small href={`https://bscscan.com/address/${account}`} className="mb-2">
+              View on Klaytnscope
+            </LinkExternal>
+            <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
+            <Button
+              size="sm"
+              variant="secondary"
+              className="mt-4"
+              onClick={() => {
+                logout()
+                window.localStorage.removeItem(localStorageKey)
+                window.location.reload()
+              }}
+            >
+              Disconnect
+            </Button>
+          </div>
+        </Dropdown>
       ) : (
         <Button
           size="sm"
           onClick={() => {
-            onPresentConnectModal();
+            onPresentConnectModal()
           }}
         >
           Connect
         </Button>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default UserBlock;
+export default UserBlock
