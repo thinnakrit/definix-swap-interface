457f36fe4e3d2f5c3e0fd89f46f3763cc859a33b
Laddawal Kongwong <laddawal.kongwong>
2021-04-02 11:33:24 +0700

    Add, Minus Icon

--- src/uikit-dev/components/Svg/Icons/Minus.tsx
+++ src/uikit-dev/components/Svg/Icons/Minus.tsx
@@ -4,11 +4,8 @@ import { SvgProps } from '../types'
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 14 1.2" {...props}>
-      <path
-        d="M13.2,1H1.2C.65,1,.2.775.2.5S.65,0,1.2,0h12c.55,0,1,.225,1,.5S13.75,1,13.2,1Z"
-        transform="translate(-0.2)"
-      />
+    <Svg viewBox="0 0 24 24" {...props}>
+      <path d="M18,13H6a1,1,0,0,1,0-2H18a1,1,0,0,1,0,2Z" />
     </Svg>
   )
 }
